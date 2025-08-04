50b8a915b617c56936f7f04a6759e5d04790af58
zacaco <silveira.santos.paulo@escola.pr.gov.br>
2025-08-04 09:30:59 -0300

    criado um arquivo com exemplos de variaveis e condicionais

---
+++ variaveis-condicionais.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+//criando variaveis ded inteiro
+int main() {
+    int x = 20;
+    int y = 18;
+    
+    //este é um conficional
+  if ( x > y) {
+         print("20 é maior que 18");   
+  }
+   
+   //-----------------------//------------------------//
+   int horario = 20;
+       if (horario < 18;){
+           print("bom dia, piazada!")
+       } else{
+           print("boa noite")
+       }
+       
+    return 0;ç
+}
